ff3978069b2630757a139f1c836f275b8c59e6db
Sai Mangipudi <thegeekskm101@gmail.com>
2025-10-17 21:49:00 -0500

    fixed font issue

--- src/data/loadAssets.ts
+++ src/data/loadAssets.ts
@@ -3,5 +3,5 @@ import type { KAPLAYCtx } from "kaplay";
 export function LoadAssets(k: KAPLAYCtx) : void
 {
     k.loadSprite("spr_dice", "/art/spr_dice.png");
-    k.loadFont("monogram", "/font/monogram-extended.ttf");
+    k.loadBitmapFont("monogram", "font/monogram-bitmap.png", 6, 12);
 }
